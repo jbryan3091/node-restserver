dd6a521a7b951c4ce3f37d0f23cbe60ffa61f1db
jim <jbryan3091@gmail.com>
2020-08-20 02:57:53 +0200

    RestServer-Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,23 @@
-//===================
-// Puerto
-//===================
-
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://jdios:kA75aTSDeAbq14mL@cafe.y3fle.mongodb.net/cafe' //process.env.MONGO_URI;
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,54 +1,32 @@
 require('./config/config');
 
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-    // parse application/x-www-form-urlencoded
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario - Local')
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err, res) => {
 
-    } else {
-        res.json({
-            persona: body
-        })
-    }
+    if (err) throw err;
 
-    res.json({
-        body
-    });
+    console.log('Base de datos ONLINE');
 
 });
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-});
-
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando 3000');
+    console.log('Escuchando puerto: ', process.env.PORT);
 });
